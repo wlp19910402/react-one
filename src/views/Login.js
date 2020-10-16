796d719b0c747b9d5cd1211966510520915a3099
王莉萍 <5556929+wlp6897@user.noreply.gitee.com>
2020-10-16 17:23:22 +0800

    登录页面添加接口

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,23 +1,60 @@
 import React from 'react';
-import { List, InputItem, WhiteSpace ,Button,Toast,WingBlank,Flex} from 'antd-mobile';
+import { List, InputItem, WhiteSpace ,Button,WingBlank,Flex,Toast} from 'antd-mobile';
 import { createForm } from 'rc-form';
 import {USER_IDNTITY,getIdntityName,getUnIdntityName} from '@/plugins/resurceStatus/user'
-
+import {login} from '@/plugins/requestServer/user'
+import {regexp} from '@/plugins/common/regexp'
 class BasicInputExample extends React.Component {
   constructor(...args) {
 	  super(...args);
 		this.state={
-			loginUserIdentity:USER_IDNTITY.BANK_STAFF
+      loginUserIdentity:USER_IDNTITY.BANK_STAFF,
+      accountId:"",
+      passowrd:"",
+      hasError:""
 		}
-	}
+  }
+  componentDidMount(){
+    console.log(9)
+  }
   handleClick = () => {
-    Toast.info('请输入账号和密码', 1);
+    const { getFieldValue } = this.props.form;
+    const accountId = getFieldValue('accountId');
+    const password = getFieldValue('pwd');
+    if(accountId===undefined||password===undefined){
+      Toast.info('账号和密码不能为空', 1);
+      return
+    }
+    if(!regexp.regMobile.test(accountId)){
+      Toast.info('请输入正确的11位手机号', 1);
+      return
+    }
+    const data = {
+      accountId,
+      password,
+      accountType: this.state.loginUserIdentity}
+    //请求登录
+    login(data)
   }
   //切换身份登录
   switchIndtify=()=>{
     this.setState({
       loginUserIdentity:this.state.loginUserIdentity===USER_IDNTITY.FOREIGN_STAFF?USER_IDNTITY.BANK_STAFF:USER_IDNTITY.FOREIGN_STAFF
     })
+  }
+  onChange = (value) => {
+    this.setState({
+      accountId: value,
+    });
+    if (value.replace(/\s/g, '').length < 11) {
+      this.setState({
+        hasError: true,
+      });
+    } else {
+      this.setState({
+        hasError: false,
+      });
+    }
   }
   render() {
     const { getFieldProps } = this.props.form;
@@ -28,21 +65,17 @@ class BasicInputExample extends React.Component {
         </div>
         <Flex justify="center">
           <Flex.Item>
-                <WingBlank size="lg" >
+                <WingBlank size="lg">
                 <List>
-                  <InputItem  {...getFieldProps('password1')}  placeholder="账号" type="text"></InputItem>
-                  </List>
-                  <List>
-                  <InputItem
-                    {...getFieldProps('password')}
-                    type="password"
-                    placeholder="密码"
-                  ></InputItem>
+                  <InputItem {...getFieldProps('accountId')}  placeholder="账号"  type="text"></InputItem>
+                </List>
+                <List>
+                  <InputItem {...getFieldProps('pwd')} type="password" placeholder="密码"></InputItem>
                 </List>
                 <WhiteSpace size="xl"/>
                 <WhiteSpace size="xl"/>
                 <WhiteSpace size="xl"/>
-                <Button type="primary" onClick={this.handleClick}>登录</Button>
+                <Button type="primary" onClick={this.handleClick.bind(this)}>登录</Button>
                 <WhiteSpace size="md"/>
                 <Button type="default" onClick={this.switchIndtify.bind(this)}>切换为“{getUnIdntityName(this.state.loginUserIdentity)}人员”</Button>
                 </WingBlank>
